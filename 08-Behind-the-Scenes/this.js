29742fd2c0305f3b4f443aa763fffd483763f8d0
worthysteve <danielsteven.ds@gmail.com>
2024-05-30 13:11:43 +0300

    recent commit

--- 08-Behind-the-Scenes/this.js
+++ 08-Behind-the-Scenes/this.js
@@ -3,7 +3,7 @@ console.log(this); // the window object - global obj
 
 const calculateAge = function (birthYear) {
   console.log(2037 - birthYear);
-  console.log(this); //undefined;
+  // console.log(this); //undefined;
 };
 calculateAge(1995);
 
@@ -12,3 +12,120 @@ const calcAgeArrow = birthYear => {
   console.log(this); //the window object
 };
 calcAgeArrow(1999);
+
+const steven = {
+  year: 1995,
+  calculateAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+steven.calculateAge();
+
+// method borrowing
+const bex = {
+  year: 2017,
+};
+
+bex.calculateAge = steven.calculateAge;
+bex.calculateAge();
+
+// const f = steven.calculateAge();
+// f(); // returns an error
+
+console.log(
+  '======================== REGULAR FUNCTIONS vs ARROW FUNCTIONS ======================='
+);
+const daniel = {
+  firstName: 'Steven',
+  year: 1995,
+  calculateAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // solution 1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(this); // undefined
+    //   // console.log(this.year >= 1981 && this.year <= 1996); // type error
+    //   console.log(self.year >= 1981 && self.year <= 1996); // true
+
+    // };
+
+    //solution 2 - an arrow function inherits the this keyword from its parent scope
+    const isMillenial = () => {
+      console.log(this); // returns the function
+      console.log(this.year >= 1981 && this.year <= 1996); // true
+    };
+
+    isMillenial();
+  },
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+daniel.greet(); // undefined - arrow fxn does not get its own "this" keyword but from its surrounding
+daniel.calculateAge();
+
+// argument keyword
+const addExp = function (a, b) {
+  return a + b;
+};
+console.log(addExp(2, 5));
+console.log(addExp(2, 5, 8, 12));
+
+// var addArrow = (a, b) => a + b;
+
+console.log(
+  '======================== PRIMITIVES vs OBJECTS (Primitives vs Reference types) ======================='
+);
+
+let age = 28;
+let oldAge = age;
+age = 29;
+console.log(age); //29
+console.log(oldAge); // 28
+
+const mySlef = {
+  name: 'Steven',
+  age: 28,
+};
+const friend = mySlef;
+friend.age = 27;
+console.log('Friend', friend); // 27
+console.log('Me', mySlef); // 27;
+
+let lastName = 'Barrie';
+let oldLastName = lastName;
+lastName = 'Daniel';
+
+console.log(lastName); //Daniel
+console.log(oldLastName); // Barrie
+
+const umu = {
+  firstName: 'Umu-Hawa',
+  lastName: 'Barrie',
+  age: 24,
+};
+const marriedUmu = umu;
+marriedUmu.lastName = 'Daniel';
+console.log('Before marriage', umu); // Daniel - this happens because the marriedUmu obbject is referenced in memory
+console.log('After marriage', marriedUmu); // Daniel
+
+// copying object
+const umu2 = {
+  firstName: 'Umu-Hawa',
+  lastName: 'Barrie',
+  age: 24,
+  family: ['Adama', 'Hannah'],
+};
+const umuCopy = Object.assign({}, umu2);
+umuCopy.lastName = 'Daniel';
+
+//using object.assign will only work on the first level, it only creates a shallow copy.
+console.log('Before marriage', umu2); // Barrie
+console.log('After marriage', umuCopy); // Dabiel
+
+// a change in one array also changes in the other one, this means the obbject.assign only does a shallow copy
+umuCopy.family.push('Gibo');
+umuCopy.family.push('Audrey');
+console.log('Before marriage', umu2); // Barrie
+console.log('After marriage', umuCopy); // Dabiel
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -26,4 +26,193 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, menuIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[menuIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
+
+console.log(
+  '=========================== DESTRUCTURING ARRAYS ======================='
+);
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr; // this is not array but destructuring array
+console.log(x, y, z); // 2,3,4
+
+// destructuring the restaurant array
+// const [first, second] = restaurant.categories; // italian, Pizzeria
+
+// if we want the first and third elemnt, we use ',' between first and sencond
+const [first, , second] = restaurant.categories; // italian, vagetarian
+console.log(first, second);
+
+// switch the main and secondary categories
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//! switching variables without destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); // vagetarian, italian
+
+//! with destructuring
+[main, , secondary] = [secondary, , main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse); // garlic bread, pizza
+
+// nested destructuring
+const nested = [2, 4, [5, 6]];
+const [i, , j] = nested;
+console.log(i, j); // [5,6]
+const [k, , [l, m]] = nested;
+console.log(k, l, m); // [2,5,6]
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); // 8,9,1
+
+//! DESTRUCTURING OBJECTS
+console.log(
+  '=========================== DESTRUCTURING OBJECTS ======================='
+);
+// to destructure object, we use the curley {} braces
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters); // returns an enpty array for menu but returns the starter menu for starters
+
+// Mutating variables while destructuring objects
+let d = 111;
+let e = 999;
+const obj = { d: 23, e: 7, f: 14 };
+// {e,d}=obj // syntax error - JS expect a code block
+({ e, d } = obj);
+// to solve, store in paranrthesis
+console.log(e, d); // 7, 23
+
+// nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close); // 11 23
+
+console.log(
+  restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+  })
+);
+
+console.log(
+  restaurant.orderDelivery({
+    address: 'Via del sole, 21',
+    starterIndex: 1,
+  })
+);
+
+console.log(
+  '=========================== THE SPREAD OPERATOR ======================='
+);
+
+const ar = [7, 8, 9];
+const badNewArr = [1, 2, ar[0], ar[1], ar[2]];
+console.log(badNewArr); // [1,2,7,8,9]
+
+//the spread operator
+const newArr = [1, 2, ...ar];
+console.log(newArr); // [1,2,7,8,9]
+
+console.log(...newArr); // 1,2,7,8,9
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const combinedMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(combinedMenu);
+
+// Iterables: arrays, strings, maps, sets, NOT objects
+const str = 'Steven';
+const letters = [...str, ' ', 's.'];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Daniel`); // error
+
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt('Ingredient 2?'),
+  // prompt('Ingredient 3?'),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients); // Here is your delicious pasta with mushrooms, cheese and spaghetti
+
+// Objects
+const newRestaurant = {
+  foundingYear: 2022,
+  ...restaurant,
+  founder: 'Umu-Hawa',
+};
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Umu's Eatery";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+console.log(
+  '=========================== THE REST PATTERN ======================='
+);
+// the spread patter is used to unpack an element while the rest pattern is used to pack an element
+
+// SPREAD, because on RIGHT side of =
+const arrr = [1, 2, ...[3, 4]];
+
+// SPREAD, because on LEFT side of =
+const [g, h, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
