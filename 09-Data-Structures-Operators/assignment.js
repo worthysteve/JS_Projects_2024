60b8bb2cef776d4cc82be808c133e5b599c97c09
worthysteve <danielsteven.ds@gmail.com>
2024-05-30 15:31:49 +0300

    commit

--- 09-Data-Structures-Operators/assignment.js
+++ 09-Data-Structures-Operators/assignment.js
@@ -401,3 +401,44 @@ const spellWord = function (word) {
   console.log(...word);
 };
 spellWord('JavaScript');
+
+console.log(
+  '================================ THE REST PATTERN AND PARAMETER ========================='
+);
+
+/* 4.1
+Destructure the keywords property (array) of
+ the first book from the books array into 
+ variables called mainKeyword and rest. 
+ The first keyword should be assigned to
+  mainKeyword, and the rest of the keywords
+   should be assigned to the rest 
+   variable (it should be an array).*/
+
+const [mainKeyword, ...rest] = books[0].keywords;
+console.log(mainKeyword, rest);
+
+/* 4.2
+Destructure the second book from the books
+ array into a variable called bookPublisher. 
+ The bookPublisher variable should be assigned
+  with the value of the publisher property
+   of the book object. Assign the rest of
+    the properties to the restOfTheBook variable.*/
+
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+console.log(bookPublisher, restOfTheBook);
+
+/* 4.3
+Write a function called printBookAuthorsCount
+ that has two parameters called title and authors. 
+ The authors parameter should accept any number
+  of arguments. This function should log to the
+   console a string formatted like that: 
+   "The book "${title}" has ${authors.length} authors".*/
+
+const printBookAuthorsCount = function (title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+};
+
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -47,6 +47,11 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 console.log(
@@ -202,6 +207,8 @@ console.log(restaurant.name);
 console.log(
   '=========================== THE REST PATTERN ======================='
 );
+
+//! DESTRUCTING
 // the spread patter is used to unpack an element while the rest pattern is used to pack an element
 
 // SPREAD, because on RIGHT side of =
@@ -216,3 +223,23 @@ const [pizza, , risotto, ...otherFood] = [
   ...restaurant.starterMenu,
 ];
 console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//! FUNCTIONS
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const s = [23, 5, 7];
+add(...s);
+
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms'); // otherIngredients will have an empty array
